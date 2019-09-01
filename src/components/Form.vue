332fbf976c36f9fb998e9619632fb2bb753f9d36
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2019-09-01 16:40:31 +0200

    aaa

---
+++ src/components/Form.vue
@@ -0,0 +1,43 @@
+
+<template>
+	<form>
+		<label for="first_name">First Name</label>
+		<input name="first_name" type="text" placeholder="Enter first name">
+
+		<div class="buttonset">
+			<button>Submit</button>
+		</div>
+	</form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+	form {
+		display: inline-flex;
+		flex-direction: column;
+		align-items: flex-start;
+	}
+	input {
+		border: 2px solid #999;
+		padding: 15px;
+		margin-bottom: 15px;
+	}
+	.buttonset {
+		display: flex;
+		width: 100%;
+		justify-content: flex-end;
+	}
+	button {
+		margin: 0;
+		border: none;
+		background: #66b;
+		color: #fff;
+		padding: 15px;
+	}
+</style>
+
