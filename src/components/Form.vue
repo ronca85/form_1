56149493493ff3377ba10e9fbf5b1089aaf3b2a4
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2019-09-01 19:53:16 +0200

    aaa

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,18 +1,34 @@
 
 <template>
-	<form>
-		<label for="first_name">First Name</label>
-		<input name="first_name" type="text" placeholder="Enter first name">
-
+	<form v-on:submit.prevent="onSubmit" :class="['', validateEmail()]">
+		<label>Email</label>
+		<input type="email" v-model="user.email"/>
 		<div class="buttonset">
-			<button>Submit</button>
+			<button type="submit">Submit</button>
 		</div>
 	</form>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			user : {
+				email : "",
+				reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
+			}
+		}
+	},
 
+	methods: {
+		validateEmail: function() {
+			return (this.user.email == "")? "" : (this.user.reg.test(this.user.email)) ? 'has-success' : 'has-error';
+		},
+		onSubmit() {
+			console.log( "submit" );
+			this.validateEmail;
+		}
+	},
 }
 </script>
 
